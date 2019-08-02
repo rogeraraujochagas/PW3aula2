5f7ed754b8ab0027ad161df6593a0cb0bb92d9eb
Rogerio <rogeri_22@hotmail.com>
2019-08-02 20:31:48 -0300

    Exercicio 2 - TypeScript

---
+++ dist/exercicio1.js
@@ -0,0 +1,11 @@
+var nota1 = 10;
+var nota2 = 20;
+var nota3 = 30;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A média é " + media);
+if (media >= 7) {
+    console.log("APROVADO");
+}
+else {
+    console.log("REPROVADO");
+}
