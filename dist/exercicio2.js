dbeeeb744ba6288660e66cd6702af1bb7a194fa5
Rogerio <rogeri_22@hotmail.com>
2019-08-02 21:49:35 -0300

    Exxercicio 2

---
+++ dist/exercicio2.js
@@ -0,0 +1,11 @@
+{
+    var comp = 10;
+    var qtdFolhas = 90;
+    var folhasComp = 10;
+    if (comp * folhasComp > qtdFolhas) {
+        console.log("NÃO");
+    }
+    else {
+        console.log("SIM");
+    }
+}
---
+++ exercicio2.ts
@@ -0,0 +1,14 @@
+{
+    let comp: number = 10
+    let qtdFolhas: number = 90
+    let folhasComp: number = 10
+
+    
+
+    if(comp * folhasComp > qtdFolhas){
+        console.log("NÃO")
+    }
+    else{
+        console.log("SIM")
+    }
+}
