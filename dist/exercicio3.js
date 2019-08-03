31c421785e29d5660655cce0eae25b9dc9b496d1
Rogerio <rogeri_22@hotmail.com>
2019-08-02 22:31:39 -0300

    Exercicio 3

---
+++ dist/exercicio3.js
@@ -0,0 +1,9 @@
+var n1 = 10;
+var n2 = 20;
+var n3 = 30;
+//let media: number = (nota1 + nota2 + nota3) /3
+/*let fMedia = function(nota1:number, nota2:number, nota3:number) : number{
+    return (nota1 + nota2 + nota3) /3
+}*/
+var fMedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
+console.log(" A m\u00E9dia \u00E9  " + fMedia(n1, n2, n3));
